6258ad0630728ce9640073915b1eb2486901ad01
Nodroh-fash <officialadijat@gmail.com>
2022-12-08 03:58:29 -0800

    messag

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point print a line of code using puts
+ * Main -print a line of code using puts
  *
  * Return: 0
 */
 
 int main(void)
 {
-     
-     	puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}       
+	puts( "\"Programming is like building a multilingual puzzle");
+        return (0);
+}	
+		
