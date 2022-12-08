33ba13c07354e17d74c9b4545a8767b3d9e2e70a
Nodroh-fash <officialadijat@gmail.com>
2022-12-08 02:29:59 -0800

    firs c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point print a line of code using puts
  *
- * Return: Always  o
+ * Return: 0
 */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle"):
-       return (o);	       
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}       
